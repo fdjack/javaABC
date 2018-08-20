fbdda100ac50998656acadc143a319cbab7ff215
zy <zhangyi170cc@163.com>
2018-08-20 09:24:12 +0800

    感谢开源大佬，5分钟做好快速二维码生成器—__—

--- Zxing/src/main/java/com/zy/ZxingDemo.java
+++ Zxing/src/main/java/com/zy/ZxingDemo.java
@@ -1,6 +1,12 @@
 package com.zy;
 
+import com.zy.utils.QRCodeUtil;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.UUID;
 
 /**
  * @author: zhangyi
@@ -10,9 +16,22 @@ import org.junit.Test;
 public class ZxingDemo {
 
     @Test
-    public void fun(){
-        String url1 = "";
-        String url2 = "";
-        String url3 = "";
+    public void fun() throws Exception{
+        String[] urls = new String[]{
+                "http://118.31.228.186/apk/yishusmt.apk",
+                "https://yishusmt.oss-cn-hangzhou.aliyuncs.com/yishusmt_mob/apk/yishusmt.apk"
+        };
+        //生成后的图片路径
+        String dir = "E:\\smt-api\\erweima\\";
+        //二维码自带logo图片路径
+        String logoImgPath = "E:\\smt-api\\erweima\\icon.png";
+        File file = new File(dir);
+        Arrays.stream(urls).forEach(url -> {
+            try {
+                QRCodeUtil.encode(url, logoImgPath, new FileOutputStream(file+"\\"+UUID.randomUUID()+".jpg"), true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
---
+++ Zxing/src/main/java/com/zy/utils/FileUtil.java
@@ -0,0 +1,32 @@
+package com.zy.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
+/**
+ * @author: zhangyi
+ * @date: 2018/8/20 09:00
+ * @description:
+ */
+public class FileUtil {
+    /**
+     * user: Rex
+     * date: 2016年12月29日  上午12:25:04
+     * @param dir
+     * void
+     * TODO 判断路径是否存在，如果不存在则创建
+     */
+    public static void mkdirs(String dir){
+        if(StringUtils.isEmpty(dir)){
+            return;
+        }
+
+        File file = new File(dir);
+        if(file.isDirectory()){
+            return;
+        } else {
+            file.mkdirs();
+        }
+    }
+}
